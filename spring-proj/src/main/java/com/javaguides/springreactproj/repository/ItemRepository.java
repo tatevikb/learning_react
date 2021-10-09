1ec52080716df10272ef56e3e1d9a0dea12f9a0a
tatevikb <tatevikm.badalyan@gmail.com>
2021-10-10 01:41:09 +0400

    Removes all files

--- spring-proj/src/main/java/com/javaguides/springreactproj/repository/ItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.javaguides.springreactproj.repository;
-
-import com.javaguides.springreactproj.model.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ItemRepository extends JpaRepository<Item, Long> {
-}
--- spring-proj/src/main/java/com/javaguides/springreactproj/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.javaguides.springreactproj.repository;
-
-import com.javaguides.springreactproj.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
